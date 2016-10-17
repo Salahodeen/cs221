93995bca9784a594ae7e6627874bbd408f624627
salahodeen <salahodeen@192.168.1.10>
2016-10-16 23:19:47 -0700

    Test commit from Mac

--- reconstruct/submission.py
+++ reconstruct/submission.py
@@ -7,6 +7,7 @@ import wordsegUtil
 
 class SegmentationProblem(util.SearchProblem):
     def __init__(self, query, unigramCost):
+        #test commit
         self.query = query
         self.unigramCost = unigramCost
 
